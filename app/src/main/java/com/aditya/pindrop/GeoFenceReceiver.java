0433f292208596a46cbc97be8e7b6b67ec3c219f
aditya-bhawsar <aditya.bhawsar98@gmail.com>
2021-05-24 18:58:56 +0530

    Formatting

--- app/src/main/java/com/aditya/pindrop/GeoFenceReceiver.java
+++ app/src/main/java/com/aditya/pindrop/GeoFenceReceiver.java
@@ -1,16 +1,83 @@
 package com.aditya.pindrop;
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Build;
+import android.util.Log;
+
+import androidx.core.app.NotificationCompat;
+
+import com.google.android.gms.location.Geofence;
+import com.google.android.gms.location.GeofencingEvent;
 
 public class GeoFenceReceiver extends BroadcastReceiver {
 
     public static final String TAG = GeoFenceReceiver.class.getSimpleName();
+    public static final String NOTIFICATION_CHANNEL_ID = "Transition";
+    public static final String NOTIFICATION_CHANNEL_NAME = "GeoFence Transition";
+    public static final int NOTIFICATION_ID = 1;
 
     @Override
     public void onReceive(Context context, Intent intent) {
 
+        GeofencingEvent event = GeofencingEvent.fromIntent(intent);
+        if(event.hasError()){
+            Log.e(TAG, String.valueOf(event.getErrorCode()));
+            return;
+        }
+
+        int transition = event.getGeofenceTransition();
+
+        if(transition == Geofence.GEOFENCE_TRANSITION_ENTER){
+            //Silence System
+            notifyAndSilence(context, true);
+        }
+        else if(transition == Geofence.GEOFENCE_TRANSITION_EXIT){
+            //DeSilence System
+            notifyAndSilence(context, false);
+        }
+        else {}
+    }
+
+
+    private void notifyAndSilence(Context ctx, boolean silence){
+
+        NotificationManager notificationManager = (NotificationManager) ctx.getSystemService(Context.NOTIFICATION_SERVICE);
+        createChannel(notificationManager);
+
+        NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(ctx, NOTIFICATION_CHANNEL_ID);
+
+        String title= "";
+        String text= "";
+        if(silence) {
+            title = "Device in Silence Mode";
+            text = "You entered a marked zone silence mode is activated";
+        }
+        else {
+            title = "Device in General Mode";
+            text = "You exited a marked zone general mode is activated";
+        }
+
+        notificationBuilder.setContentTitle(title);
+        notificationBuilder.setContentText(text);
+
+        notificationManager.notify(NOTIFICATION_ID, notificationBuilder.build());
+
+    }
+
+    private void createChannel(NotificationManager notificationManager){
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
+            NotificationChannel channel = new NotificationChannel(
+                    NOTIFICATION_CHANNEL_ID,
+                    NOTIFICATION_CHANNEL_NAME,
+                    NotificationManager.IMPORTANCE_DEFAULT
+            );
+
+            notificationManager.createNotificationChannel(channel);
+        }
     }
 
     /*@Override
@@ -80,14 +147,12 @@ public class GeoFenceReceiver extends BroadcastReceiver {
         mNotificationManager.notify(0, builder.build());
     }
 
-
     private void setRingerMode(Context context, int mode) {
         NotificationManager nm = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
         Log.e("Did it Happen","true");
         AudioManager audioManager = (AudioManager) context.getSystemService(Context.AUDIO_SERVICE);
         audioManager.setRingerMode(mode);
         Log.e("executed","done:");
-
     }*/
 
 }
