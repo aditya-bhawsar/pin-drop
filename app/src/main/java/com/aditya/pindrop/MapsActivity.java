031b913fc3198f57c598fcdf0e1bb29d888a608d
aditya-bhawsar <aditya.bhawsar98@gmail.com>
2021-08-02 11:41:27 +0530

    Formatting

--- app/src/main/java/com/aditya/pindrop/MapsActivity.java
+++ app/src/main/java/com/aditya/pindrop/MapsActivity.java
@@ -46,6 +46,7 @@ public class MapsActivity extends AppCompatActivity implements
         super.onCreate(savedInstanceState);
 
         try { Thread.sleep(700); }
+
         catch (Exception e) { e.printStackTrace(); }
 
         setTheme(R.style.Theme_PinDrop);
